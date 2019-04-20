7d94675c45c5161d27c1786cc130877bedf0ffdc
Eriq <Eriq@DESKTOP-EAB0I4Q>
2019-04-20 19:40:19 -0400

    Start restart on Input

--- Project 5 CS2114/src/prj5/Input.java
+++ Project 5 CS2114/src/prj5/Input.java
@@ -5,82 +5,23 @@ package prj5;
 
 /**
  * @author Tom
- *
+ * @author Eriq Taing (eriq12)
+ * @version 4/20/2019
  */
 public class Input {
-    
-    private SongList songList;
-    /**
-     * Read song to the songList
-     * 
-     * @param str
-     *            Address of song data
-     * @throws FileNotFoundException
-     */
-    private void readSongFile(String songData) throws FileNotFoundException 
-    {
-        Scanner input = new Scanner(new File(songData));
-        String[] str;
 
-        input.nextLine();
-
-        while (input.hasNextLine()) 
-        {
-            str = input.nextLine().trim().split(",");
-            songList.add(new Song(str[0], str[1], str[2], str[3]));
-        }
-
-        input.close();
-    }
-
-    /**
-     * Read student file and add data to songList
-     * 
-     * @param studentData
-     *            Address of student data
-     * @throws FileNotFoundException
-     */
-    private void readStudentFile(String studentData) throws FileNotFoundException 
-    {
-        Scanner input = new Scanner(new File(studentData));
-        String[] str;
-        Song song;
-        String hobby;
-        String major;
-        String region;
-
-        input.nextLine();
-
-        while (input.hasNextLine()) {
-            String[] temp = input.nextLine().trim().split(",");
-            // get rid of invalid lines
-            if (temp.length == (songList.size() * 2 + 4)) {
-                str = new String[temp.length + 1];
-                for (int i = 0; i < temp.length; i++) {
-                    str[i] = temp[i];
-                }
-                str[temp.length] = "";
+    public static void main(String[] args) {
+        FileReader reader = null;
+        if (args.length == 2) {
+            try {
+                reader = new FileReader(args[0], args[1]);
             }
-            else {
-                str = temp;
-            }
-            if (str.length <= 2) {
-                continue;
-            }
-            
-            // record the major, region, and hobby info of a student
-            major = str[2];
-            region = str[3];
-            hobby = str[4];
-            
-            // update the counts attributes of the song
-            for (int i = 5; (i + 1) < str.length; i += 2) {
-                song = songList.getAt((i - 5) / 2);
-                song.getMajorPercent().increment(major, str[i], str[i + 1]);
-                song.getRegionPercent().increment(region, str[i], str[i + 1]);
-                song.getHobbyPercent().increment(hobby, str[i], str[i + 1]);
+            catch (Exception e) {
+                e.printStackTrace();
             }
         }
-        input.close();
+        if (reader != null) {
+            
+        }
     }
 }
