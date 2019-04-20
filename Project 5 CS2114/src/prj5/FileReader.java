cb1b2c98e15b9896e70268d51e30517fdf917a77
Eriq <Eriq@DESKTOP-EAB0I4Q>
2019-04-20 19:36:47 -0400

    Amended FileReader, removed the percent classes

--- Project 5 CS2114/src/prj5/FileReader.java
+++ Project 5 CS2114/src/prj5/FileReader.java
@@ -15,42 +15,101 @@ import java.util.Scanner;
 public class FileReader {
     private LinkedList<Song> songs;
     private LinkedList<Student> students;
-    /**
-     * amount of songs
-     */
-    public static final int NUM_OF_SONGS = 59;
+
 
     /**
      * default constructor
      * 
      * @param fileName
      *            name of file to import
+     * @throws Exception
+     *             if invalid data
      */
-    public FileReader(String studentFileName, String songFileName) {
-
+    public FileReader(String studentFileName, String songFileName)
+        throws Exception {
+        songs = extractSongs(songFileName);
+        students = extractStudents(studentFileName);
     }
-    
-    private LinkedList<Student> extractStudents(String fileName) throws Exception{
+
+
+    /**
+     * 
+     * @param fileName
+     *            file location
+     * @return list of students
+     * @throws Exception
+     *             if the amount of entries doesn't correlate to amount of songs
+     */
+    private LinkedList<Student> extractStudents(String fileName)
+        throws Exception {
         LinkedList<Student> result = new LinkedList<Student>();
         Scanner sc = new Scanner(new File(fileName));
-        while(sc.hasNextLine()){
+        sc.nextLine();
+        while (sc.hasNextLine()) {
             String[] data = sc.nextLine().split(",");
-            if(data.length != (5 + NUM_OF_SONGS * 2)){
-                throw new Exception("Invalid given data, improper amount of entries");
+            if (data.length != (5 + songs.size() * 2)) {
+                throw new Exception(
+                    "Invalid given data, improper amount of entries");
             }
             int index = Integer.valueOf(data[0]);
             String date = data[1];
-            Major maj = Major.valueOf((data[2].replaceAll(" ", "_")));
-            Region reg = Region.valueOf(data[3].toUpperCase());
+            Major maj = Major.valueOf((data[2].replaceAll(" ", "_").toUpperCase()));
+            Region reg = Region.valueOf(data[3].replaceAll(" ", "_").toUpperCase());
             Hobby hob = Hobby.valueOf(data[4].toUpperCase());
-            boolean[][] responses = new boolean[NUM_OF_SONGS][2];
-            for(int i = 0; i < responses.length * 2; i++){
-                responses[i/2][i%2] = Boolean.valueOf(data[i+5]); 
+            Response[][] responses = new Response[songs.size()][2];
+            for (int i = 0; i < responses.length * 2; i++) {
+                try {
+                    responses[i / 2][i % 2] = Response.valueOf(data[i + 5]);
+                }
+                catch (Exception e) {
+                    responses[i / 2][i % 2] = Response.NEITHER;
+                }
             }
-            Student newEntry = new Student(index, date, maj, reg, hob, responses);
+            Student newEntry =
+                new Student(index, date, maj, reg, hob, responses);
             result.add(newEntry);
         }
         return result;
+    }
+
+
+    /**
+     * Parses through the data to return a linked list
+     * of all of the songs
+     * 
+     * @param fileName
+     *            name of input file
+     * @return LinkedList with all the songs
+     * @throws Exception
+     *             through missing file exception
+     */
+    private LinkedList<Song> extractSongs(String fileName) throws Exception {
+
+        LinkedList<Song> result = new LinkedList<Song>();
+        Scanner sc = new Scanner(new File(fileName));
+        sc.nextLine();
+
+        while (sc.hasNextLine()) {
+
+            String[] data = sc.nextLine().split(",");
+            int index = 0;
+            Song newSong =
+                new Song(data[0], data[1], Integer.valueOf(data[2]), data[3],
+                    index);
+            result.add(newSong);
+            index++;
+        }
+        return result;
+    }
+
+
+    /**
+     * get amount of songs
+     * 
+     * @return number of songs
+     */
+    public int numOfSongs() {
+        return songs.size();
     }
 
 }
