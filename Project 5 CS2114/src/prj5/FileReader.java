41e493c76f50c0024b22fe121ff99ef859ce4f5d
thimler9 <49621649+thimler9@users.noreply.github.com>
2019-04-20 20:17:34 -0400

    Update FileReader.java

--- Project 5 CS2114/src/prj5/FileReader.java
+++ Project 5 CS2114/src/prj5/FileReader.java
@@ -4,7 +4,6 @@
 package prj5;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
@@ -15,101 +14,145 @@ import java.util.Scanner;
 public class FileReader {
     private LinkedList<Song> songs;
     private LinkedList<Student> students;
-
+    /**
+     * amount of songs
+     */
+    public int numOfSongs;
 
     /**
      * default constructor
      * 
      * @param fileName
      *            name of file to import
-     * @throws Exception
-     *             if invalid data
      */
-    public FileReader(String studentFileName, String songFileName)
-        throws Exception {
-        songs = extractSongs(songFileName);
-        students = extractStudents(studentFileName);
+    public FileReader(String studentFileName, String songFileName) {
+        try {
+            songs = extractSongs(songFileName);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            students = extractStudents(studentFileName);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
-
-
+    
     /**
+     * Parses through the students file and puts them in a 
+     * LinkedList
      * 
-     * @param fileName
-     *            file location
-     * @return list of students
-     * @throws Exception
-     *             if the amount of entries doesn't correlate to amount of songs
+     * Note: Students will be null if the number of songs in the song
+     * file is different than the number of songs assumed in the student
+     * file.
+     * 
+     * @param fileName name of student file
+     * @return a linked list of the students
+     * @throws Exception through missing file exception
      */
-    private LinkedList<Student> extractStudents(String fileName)
+    @SuppressWarnings("resource")
+    private LinkedList<Student> extractStudents(String fileName) 
         throws Exception {
+        
         LinkedList<Student> result = new LinkedList<Student>();
         Scanner sc = new Scanner(new File(fileName));
-        sc.nextLine();
-        while (sc.hasNextLine()) {
+        sc.nextLine(); //Skipping the headers line
+        
+        while(sc.hasNextLine()) {
+            
             String[] data = sc.nextLine().split(",");
-            if (data.length != (5 + songs.size() * 2)) {
-                throw new Exception(
-                    "Invalid given data, improper amount of entries");
-            }
             int index = Integer.valueOf(data[0]);
             String date = data[1];
-            Major maj = Major.valueOf((data[2].replaceAll(" ", "_").toUpperCase()));
-            Region reg = Region.valueOf(data[3].replaceAll(" ", "_").toUpperCase());
-            Hobby hob = Hobby.valueOf(data[4].toUpperCase());
-            Response[][] responses = new Response[songs.size()][2];
-            for (int i = 0; i < responses.length * 2; i++) {
-                try {
-                    responses[i / 2][i % 2] = Response.valueOf(data[i + 5]);
-                }
-                catch (Exception e) {
-                    responses[i / 2][i % 2] = Response.NEITHER;
+            Major maj = null;
+            Region reg = null;
+            Hobby hob = null;
+            Response[][] responses = new Response[numOfSongs][2];
+            
+            for (int i = 0; i < numOfSongs; i++) {
+                for (int j = 0; j < 2; j++) {
+                    responses[i][j] = Response.NEITHER;
                 }
             }
-            Student newEntry =
-                new Student(index, date, maj, reg, hob, responses);
+            
+            //Assigning the maj, reg, and hob values
+            if (data.length > 2) {
+                if (!data[2].equals("")) {
+                    maj = Major.valueOf(data[2].replaceAll(" ", "_").toUpperCase());
+                }
+                if (!data[3].equals("")) {
+                    reg = Region.valueOf(data[3].replaceAll(" ",  "_")
+                        .replaceAll("[()]", "").toUpperCase());
+                }
+                if (!data[4].equals("")) {
+                    hob = Hobby.valueOf(data[4].toUpperCase());
+                }
+            }
+            
+            //The true or falses being put into the response array
+            for(int i = 5; i < data.length; i++) {
+                if (data[i].equals("Yes")) {
+                    responses[(i - 5) / 2][i % 2] = Response.YES;
+                }
+                else if (data[i].equals("No")) {
+                    responses[(i - 5) / 2][i % 2] = Response.NO;
+                }
+            }
+            
+            Student newEntry = new Student(index, date, maj, 
+                reg, hob, responses);
             result.add(newEntry);
         }
         return result;
     }
-
-
+    
     /**
      * Parses through the data to return a linked list
      * of all of the songs
      * 
-     * @param fileName
-     *            name of input file
+     * @param fileName name of input file
      * @return LinkedList with all the songs
-     * @throws Exception
-     *             through missing file exception
+     * @throws Exception through missing file exception
      */
-    private LinkedList<Song> extractSongs(String fileName) throws Exception {
-
+    @SuppressWarnings("resource")
+    private LinkedList<Song> extractSongs(String fileName) 
+        throws Exception {
+        
         LinkedList<Song> result = new LinkedList<Song>();
         Scanner sc = new Scanner(new File(fileName));
         sc.nextLine();
-
+        
         while (sc.hasNextLine()) {
-
+            
             String[] data = sc.nextLine().split(",");
             int index = 0;
-            Song newSong =
-                new Song(data[0], data[1], Integer.valueOf(data[2]), data[3],
-                    index);
+            Song newSong = new Song(data[0], data[1], 
+                Integer.valueOf(data[2]), data[3], index);
             result.add(newSong);
             index++;
         }
+        numOfSongs = result.getSize();
         return result;
     }
-
-
+    
     /**
-     * get amount of songs
+     * Gets the extracted songs
      * 
-     * @return number of songs
+     * @return songs
      */
-    public int numOfSongs() {
-        return songs.size();
+    public LinkedList<Song> getSongs()
+    {
+        return songs;
     }
-
+    
+    /**
+     * Gets the extracted students
+     * 
+     * @return students
+     */
+    public LinkedList<Student> getStudents()
+    {
+        return students;
+    }
 }
