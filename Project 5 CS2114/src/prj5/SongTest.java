364df81f378daed138aa1deb16931e7dab5c6b5e
eriqvt <49623106+eriqvt@users.noreply.github.com>
2019-04-22 13:14:43 -0400

    Update SongTest.java
    incomplete but operable, need to expand on this

--- Project 5 CS2114/src/prj5/SongTest.java
+++ Project 5 CS2114/src/prj5/SongTest.java
@@ -15,72 +15,86 @@ public class SongTest extends TestCase {
     private Song song;
     private Song song1;
 
+
     /**
-    * This sets up for rest of tests
-    */
+     * This sets up for rest of tests
+     */
     public void setUp() {
         song = new Song("Happy", "Mike", 1980, "Rock", 0);
         song1 = new Song("Sad", "John", 1982, "Pop", 1);
     }
 
+
     /**
-    * checks if getter works
-    */
+     * checks if getter works
+     */
     public void testGetArtist() {
         assertEquals("Mike", song.getArtist());
     }
 
+
     /**
-    *  checks if getter works
-    */
+     * checks if getter works
+     */
     public void testGetSong() {
         assertEquals("Happy", song.getSong());
     }
 
+
     /**
-    *  checks if getter works
-    */
+     * checks if getter works
+     */
     public void testGetYear() {
         assertEquals(1980, song.getYear());
     }
 
+
     /**
-    *  checks if getter works
-    */
+     * checks if getter works
+     */
     public void testGetGenre() {
         assertEquals("Rock", song.getGenre());
     }
 
+
     /**
-    *  checks if getter works
-    */
+     * checks if getter works
+     */
     public void testGetIndex() {
         assertEquals(0, song.getIndex());
     }
+
+
     /**
-    *  checks if Comparator works
-    */
+     * checks if Comparator works
+     */
     public void testGetCompareByArtist() {
         Comparator<Song> comparor = song.getCompareByArtist();
         assertEquals(3, comparor.compare(song, song1));
     }
+
+
     /**
-    *  checks if comparator works
-    */
+     * checks if comparator works
+     */
     public void testGetCompareByDate() {
         Comparator<Song> comparor = song.getCompareByDate();
         assertEquals(-2, comparor.compare(song, song1));
     }
+
+
     /**
-    *  checks if comparator works
-    */
+     * checks if comparator works
+     */
     public void testGetCompareByGenre() {
         Comparator<Song> comparor = song.getCompareByGenre();
         assertEquals(2, comparor.compare(song, song1));
     }
+
+
     /**
-    *  checks if comparator works
-    */
+     * checks if comparator works
+     */
     public void testGetCompareBySongTitle() {
         Comparator<Song> comparor = song.getCompareBySongTitle();
         assertEquals(-11, comparor.compare(song, song1));
